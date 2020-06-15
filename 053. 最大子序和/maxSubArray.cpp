e005d2dbd8f2ccb3c7ca7e9f92bb303d8b2a9c3e
bhsimon0810 <52440397+bhsimon0810@users.noreply.github.com>
2020-06-15 19:58:08 +0800

    Create maxSubArray.cpp

---
+++ 053. 最大子序和/maxSubArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, maxSum = nums[0];
+        for (int i = 0; i < nums.size(); ++i) {
+            if (sum > 0)
+                sum += nums[i];
+            else
+                sum = nums[i];
+            maxSum = max(sum, maxSum);
+        }
+        return maxSum;
+    }
+};
